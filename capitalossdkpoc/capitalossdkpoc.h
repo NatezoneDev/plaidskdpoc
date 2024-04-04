701f96881ff82c1db5bc68d9e639e92acda58b69
Nativ <nativbarakdev@gmail.com>
2024-04-04 07:19:36 -0400

    Initial Commit

---
+++ capitalossdkpoc/capitalossdkpoc.h
@@ -0,0 +1,18 @@
+//
+//  capitalossdkpoc.h
+//  capitalossdkpoc
+//
+//  Created by May lauren arad on 4/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for capitalossdkpoc.
+FOUNDATION_EXPORT double capitalossdkpocVersionNumber;
+
+//! Project version string for capitalossdkpoc.
+FOUNDATION_EXPORT const unsigned char capitalossdkpocVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <capitalossdkpoc/PublicHeader.h>
+
+
